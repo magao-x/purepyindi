c2b9a077e859e41b0910baeac270614a321a87da
Joseph Long <github@joseph-long.com>
2019-11-30 15:51:09 -0300

    Improved watchINDI

--- purepyindi/watchINDI.py
+++ purepyindi/watchINDI.py
@@ -1,13 +1,13 @@
-import asyncio
 import logging
+import time
 import sys
-from .eventful import AsyncINDIClient
+from .client import INDIClient
 from .constants import *
 from . import log
 from pprint import pprint
 
-async def watch_for_updates(update):
-    pprint(update)
+def watch_for_updates(prop, did_anything_change):
+    pprint(prop.to_jsonable())
 
 def main():
     import argparse
@@ -30,13 +30,21 @@ def main():
         type=int,
         default=DEFAULT_PORT,
     )
+    parser.add_argument(
+        "INDI_PROPERTY",
+        help="Dotted INDI identifier deviceName.propertyName"
+    )
     args = parser.parse_args()
     if args.help:
         parser.print_help()
         sys.exit(1)
+    print(args)
     log.set_log_level('INFO')
-    c = AsyncINDIClient(args.host, args.port)
-    c.add_async_watcher(watch_for_updates)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(c.run())
-    loop.close()
+    c = INDIClient(args.host, args.port)
+    c.start()
+    c.wait_for_properties([args.INDI_PROPERTY])
+    device_name, prop_name = args.INDI_PROPERTY.split('.')
+    watched_entity = c.devices[device_name].properties[prop_name]
+    watched_entity.add_watcher(watch_for_updates)
+    while True:
+        time.sleep(1)
